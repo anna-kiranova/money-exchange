da8e182b82ca95ddb94b166b70b0828ae811895b
Anna Kiranova <anne.kiranova@gmail.com>
2019-03-14 11:46:26 +0300

    feat: task done

--- src/index.js
+++ src/index.js
@@ -1,5 +1,23 @@
+let coins = [50, 25, 10, 5, 1];
+let coinsNames = ['H', 'Q', 'D', 'N', 'P'];
+
 // PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
+    if (currency === 0) {
+        return {};
+    }
+    if (currency > 10000) {
+        return {error: "You are rich, my friend! We don't have so much coins for exchange"};
+    }
+
+    let change = {};
+    for (let i = 0; i < coins.length; i++) {
+        let quantity = Math.floor(currency / coins[i]);
+        if (quantity > 0) {
+            currency = currency - quantity * coins[i];
+            // добавляем в объект, если количество монет больше нуля
+            change[coinsNames[i]] = quantity;
+        }
+    }
+    return change;
 }
